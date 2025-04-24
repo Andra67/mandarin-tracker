16c6e3143d1518f7ac7acd580934b73c6012f1c9
Julien Andra <julien.andra01@gmail.com>
2025-04-24 23:40:00 +0700

    Add localstorage

--- app.js
+++ app.js
@@ -6,3 +6,38 @@ const logInput= document.getElementById("logInput")
 
 let logs = JSON.parse(localStorage.getItem("mandarinLogs"))||[]
 
+
+const displayLog = () => {
+    
+
+    logs.forEach(log => {
+        const li = document.createElement("li");
+        li.innerText = `${log.date}: ${log.input}`;
+        logList.appendChild(li)
+    })
+}
+
+const addLog = (e) => {
+    e.preventDefault() 
+
+    const input = logInput.value;
+    const date = logDate.value;
+
+    if(!input || !date) {
+        alert("Please fill the log")
+    } else {
+        const li = document.createElement("li")
+
+        li.innerText = `${date}: ${input}`;
+        logList.appendChild(li)
+
+        logs.push({date, input})
+        localStorage.setItem("mandarinLogs", JSON.stringify(logs))
+
+        logInput.value = ""
+        logDate.value = ""
+    }
+
+}
+displayLog()
+logForm.addEventListener("submit", addLog)
