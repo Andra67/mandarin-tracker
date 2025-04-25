eafc8ce3942faf62617cb73c4c9887ef33f9c51c
Julien Andra <julien.andra01@gmail.com>
2025-04-25 08:41:07 +0700

    Added delete button

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ let logs = JSON.parse(localStorage.getItem("mandarinLogs"))||[]
 
 
 const displayLog = () => {
+    logList.innerHTML = ""
     
-
     logs.forEach(log => {
         const li = document.createElement("li");
+        const delBtn = document.createElement("button")
+        delBtn.innerText = "Delete Log"        
         li.innerText = `${log.date}: ${log.input}`;
+
+        delBtn.addEventListener("click", ()=> {
+            logs = logs.filter(data => !(data.input === log.input && data.date === log.date))
+            localStorage.setItem("mandarinLogs", JSON.stringify(logs))
+            displayLog();
+        })
+
+        li.appendChild(delBtn)
         logList.appendChild(li)
     })
 }
@@ -27,8 +37,12 @@ const addLog = (e) => {
         alert("Please fill the log")
     } else {
         const li = document.createElement("li")
+        const delBtn = document.createElement("button")
 
+        delBtn.innerText = "Delete Log"
         li.innerText = `${date}: ${input}`;
+
+        li.appendChild(delBtn)
         logList.appendChild(li)
 
         logs.push({date, input})
